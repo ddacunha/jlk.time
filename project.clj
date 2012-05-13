438d6a78e799f93451864ed117b815659f8238fa
JLK <taprisiot@rocketmail.com>
2012-05-13 21:39:47 +1000

    fix url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject jlk/time "0.1-SNAPSHOT"
   :description "time functions"
-  :url "https://github.com/taprisiot/jlk.utility"
+  :url "https://github.com/taprisiot/jlk.time"
   :license {:name "BSD"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.joda/time "2.0"]
