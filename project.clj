e174486346acc798ceb7e1b0fa4a2b0aed536f67
JLK <taprisiot@rocketmail.com>
2012-05-13 21:37:42 +1000

    update url and dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
 (defproject jlk/time "0.1-SNAPSHOT"
   :description "time functions"
+  :url "https://github.com/taprisiot/jlk.utility"
+  :license {:name "BSD"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.joda/time "2.0"] ;; this may be a problem later if we get to a 2.0 branch, since lein and maven seem to give them both the same name time-2.0.jar
-                 [jlk/utility "0.1"]]
+                 [org.joda/time "2.0"]
+                 ;; this may be a problem later if we get to a 2.0 branch,
+                 ;; since lein and maven seem to give them both the same name time-2.0.jar
+                 [jlk/utility "0.1-SNAPSHOT"]]
   :plugins [[lein-swank "1.4.0"]])
