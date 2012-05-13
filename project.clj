8d908c445f4c5262d8949a372c2bf35a01811ba4
JLK <taprisiot@rocketmail.com>
2012-05-13 20:57:37 +1000

    update license

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject jlk/time "0.1-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "time functions"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.joda/time "2.0"] ;; this may be a problem later if we get to a 2.0 branch, since lein and maven seem to give them both the same name time-2.0.jar
                  [jlk/utility "0.1"]]
