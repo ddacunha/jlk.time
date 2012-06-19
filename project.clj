edb6ebf34de41a9fae7f8d0d774d847efba4579c
taprisiot <taprisiot@rocketmail.com>
2012-06-19 11:40:40 +1000

    use maven central

--- project.clj
+++ project.clj
@@ -3,8 +3,6 @@
   :url "https://github.com/taprisiot/jlk.time"
   :license {:name "BSD"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.joda/time "2.0"]
-                 ;; this may be a problem later if we get to a 2.0 branch,
-                 ;; since lein and maven seem to give them both the same name time-2.0.jar
+                 [joda-time/joda-time "2.0"]
                  [jlk/utility "0.1-SNAPSHOT"]]
   :plugins [[lein-swank "1.4.0"]])
