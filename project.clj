c72dbeb43b8cc2f1ffecfaf8eca6ee6a5cfc0f28
Aaron Crow <dirtyvagabond@yahoo.com>
2013-01-25 14:02:27 -0800

    So to release a non-snapshot version

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject jlk/time "0.1-SNAPSHOT"
+(defproject factual/jlk-time "0.1"
   :description "time functions"
-  :url "https://github.com/taprisiot/jlk.time"
+  :url "https://github.com/dirtyvagabond/jlk.time"
   :license {:name "BSD"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [joda-time/joda-time "2.0"]
-                 [jlk/utility "0.1-SNAPSHOT"]]
+                 [factual/jlk-utility "0.1"]]
   :plugins [[lein-swank "1.4.0"]])
