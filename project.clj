73b9d05bb882514bd04cd1c10371e12c267da242
Daniel Da Cunha <daniel@ddc.im>
2019-10-24 08:53:44 +0800

    Updated dependencies

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject factual/jlk-time "0.1"
   :description "time functions"
-  :url "https://github.com/dirtyvagabond/jlk.time"
+  :url "https://github.com/ddacunha/jlk.time"
   :license {:name "BSD"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [joda-time/joda-time "2.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [joda-time/joda-time "2.9.3"]
                  [factual/jlk-utility "0.1"]]
   :plugins [[lein-swank "1.4.0"]])
